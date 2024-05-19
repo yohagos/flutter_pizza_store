b487d653775dc9c4668e3e5262629335765c2a66
yohagos <yosef.hagos@googlemail.com>
2024-05-19 16:08:34 +0200

    fix: import dependency and working on override functions

--- packages/user_repositories/lib/src/firebase_user_repo.dart
+++ packages/user_repositories/lib/src/firebase_user_repo.dart
@@ -1,8 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:user_repository/src/entities/entities.dart';
 import 'package:user_repository/src/models/user.dart';
 import 'package:user_repository/src/user_repo.dart';
-
 
 class FirebaseUserRepo implements UserRepository {
   final FirebaseAuth _firebaseAuth;
@@ -10,7 +11,7 @@ class FirebaseUserRepo implements UserRepository {
 
   FirebaseUserRepo({
     FirebaseAuth? firebaseAuth,
-  }): _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
 
   @override
   Future<void> logOut() {
@@ -38,7 +39,13 @@ class FirebaseUserRepo implements UserRepository {
 
   @override
   // TODO: implement user
-  Stream<MyUser?> get user => throw UnimplementedError();
-  
-
+  Stream<MyUser?> get user {
+    return _firebaseAuth.authStateChanges().flatMap((firebaseUser) async* {
+      if (firebaseUser == null) {
+        yield MyUser.empty;
+      }
+      yield await usersCollection.doc(firebaseUser?.uid).get().then((value) =>
+          MyUser.fromEntity(MyUserEntity.fromDocument(value.data()!)));
+    });
+  }
 }
