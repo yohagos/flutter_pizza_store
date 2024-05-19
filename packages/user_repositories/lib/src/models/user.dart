b8d5614da4393507acbc85a5a974cbd7a8ec6eea
yohagos <yosef.hagos@googlemail.com>
2024-05-19 15:29:15 +0200

    feat: added and fixed the user models

---
+++ packages/user_repositories/lib/src/models/user.dart
@@ -0,0 +1,51 @@
+import 'package:user_repository/src/entities/entities.dart';
+
+class MyUser {
+  String userId;
+  String email;
+  String name;
+  bool hasActiveCart;
+  /* bool hasActiveOrder;
+  String cartId;
+  String orderId;
+  String address; */
+
+  MyUser({
+    required this.userId,
+    required this.email,
+    required this.name,
+    required this.hasActiveCart,
+  });
+
+  static final empty = MyUser(
+    userId: '',
+    email: '',
+    name: '',
+    hasActiveCart: false,
+  );
+
+  MyUserEntity toEntity() {
+    return MyUserEntity(
+      userId: userId,
+      email: email,
+      name: name,
+      hasActiveCart: hasActiveCart,
+    );
+  }
+
+  static MyUser fromEntity(
+    MyUserEntity entity,
+  ) {
+    return MyUser(
+      userId: entity.userId,
+      email: entity.email,
+      name: entity.name,
+      hasActiveCart: entity.hasActiveCart,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'MyUser: $userId, $email, $name, $hasActiveCart';
+  }
+}
